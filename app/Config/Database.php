e0e223d6351f09e560034caf4aa6889113801ba4
JunjuePeng <1798751445@qq.com>
2020-10-14 13:34:10 +0800

    Add some page

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -32,7 +32,7 @@ class Database extends \CodeIgniter\Database\Config
 	public $default = [
 		'DSN'      => '',
 		'hostname' => 'localhost',
-		'username' => '',
+		'username' => 'root',
 		'password' => '',
 		'database' => '',
 		'DBDriver' => 'MySQLi',
@@ -59,10 +59,10 @@ class Database extends \CodeIgniter\Database\Config
 	 */
 	public $tests = [
 		'DSN'      => '',
-		'hostname' => '127.0.0.1',
-		'username' => '',
+		'hostname' => 'localhost',
+		'username' => 'root',
 		'password' => '',
-		'database' => ':memory:',
+		'database' => 'lab5',
 		'DBDriver' => 'SQLite3',
 		'DBPrefix' => 'db_',  // Needed to ensure we're working correctly with prefixes live. DO NOT REMOVE FOR CI DEVS
 		'pConnect' => false,
